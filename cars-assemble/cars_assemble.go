2f57e0a4b8b30f31aa6c89242ed575f8db81c923
Celine Lay <celine.lay@stoik.io>
2022-12-26 18:29:14 +0100

    Add car assemble exercise: cast

---
+++ cars-assemble/cars_assemble.go
@@ -0,0 +1,20 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) / 100 * successRate
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	nbIndividualCars := carsCount % 10
+	nbGroups := carsCount / 10
+	return uint(nbGroups*95000 + nbIndividualCars*10000)
+}
